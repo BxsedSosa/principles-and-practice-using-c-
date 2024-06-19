dc2d83ad90e7d464d02034e4d8e120b628094e61
BxsedSosa <donovanxabad@gmail.com>
2024-06-19 14:02:42 -0700

    Deleted some files

--- print_name.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-  cout << "Please enter your first name (followed by 'enter'):\n";
-  string first_name;
-  cin >> first_name;
-  cout << "Hello, " << first_name << "!\n";
-}
